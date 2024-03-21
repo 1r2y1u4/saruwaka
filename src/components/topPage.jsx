3d0a8708f52bfcd27e9d6ae2e993f6fbb67ff284
Ryusuke Hiraizumi <ryu@ryu-mba.local>
2024-03-21 14:40:18 +0900

    routeとか修正

--- src/components/topPage.jsx
+++ src/components/topPage.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import CountCharacters from './countCharacter';
+import { Link,  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './header';
+import CountCharacter from './countCharacter'
 import '../css/top-page.css';
 
 function topPage() {
   return (
-    <div className="top-page">
-      <CountCharacters />
-    </div>
-  );
+    <Router>
+        <Header />
+          <div>
+            <nav className="navigation">
+              <ul className="nav-list">
+                <li className="nav-item">
+                  <Link to="/count" className="nav-link">文字数カウント</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/conversion" className="nav-link">単位変換</Link>
+                </li>
+              </ul>
+            </nav>
+          <Routes>
+            <Route path="/count" element={<CountCharacter />} />
+            <Route path="/conversion" element={<CountCharacter />} />
+          </Routes>
+        </div>
+    </Router>
+);
 }
 
 export default topPage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import AppRoutes from './router';
 import reportWebVitals from './reportWebVitals';
+import TopPage from './components/topPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppRoutes />
+    <TopPage />
   </React.StrictMode>
 );
 
