e9509e273a5d08beae31b67a5482aa74c4a0a94f
Ryusuke Hiraizumi <ryu@ryu-mba.local>
2024-03-21 14:44:19 +0900

    indexにTOPページを設定

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
 import reportWebVitals from './reportWebVitals';
-import TopPage from './components/topPage'
+import { Link,  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import CountCharacter from './components/countCharacter'
+import './css/index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TopPage />
+    <Router>
+        <Header />
+          <div>
+            <nav className="navigation">
+              <ul className="nav-list">
+                <li className="nav-item">
+                  <Link to="/count" className="nav-link">文字数カウント</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/conversion" className="nav-link">単位変換</Link>
+                </li>
+              </ul>
+            </nav>
+          <Routes>
+            <Route path="/count" element={<CountCharacter />} />
+            <Route path="/conversion" element={<CountCharacter />} />
+          </Routes>
+        </div>
+    </Router>
   </React.StrictMode>
 );
 
